c66bd03b60fc423f18ec6e6520d637c856dfc1d1
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-06 19:47:38 +0500

    Economy.js

--- models/Economy.js
+++ models/Economy.js
@@ -1,1 +1,9 @@
+const mongoose = require("mongoose");
 
+const ecoSchema = new mongoose.Schema({
+  userId: String,
+  cash: { type: Number, default: 0 },
+  bank: { type: Number, default: 0 }
+});
+
+module.exports = mongoose.model("Economy", ecoSchema);

031125d6a6dfa65ea01f5114f6896bce6945481a
huzaifaxd9129-debug <huzaifaxd9129@gmail.com>
2026-05-06 19:47:06 +0500

    database.js

--- database.js
+++ database.js
@@ -1,1 +1,7 @@
+const mongoose = require("mongoose");
 
+module.exports = async () => {
+  await mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("🟢 MongoDB Connected"))
+    .catch(err => console.log("🔴 MongoDB Error:", err));
+};
